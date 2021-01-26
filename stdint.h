b2e78eb04092f0ed9a27726e14de07dee971814d
wangkaixuan <2654189525@qq.com>
2021-01-27 00:21:02 +0800

    delete unused data type

--- stdint.h
+++ stdint.h
@@ -1,16 +1,11 @@
 #ifndef STDINT__H
 #define STDINT__H
 
-typedef signed char int8_t;
-typedef int int16_t;
 typedef unsigned char uint8_t;
-typedef unsigned int uint16_t;
 
 #ifdef __C51__
-typedef long int int32_t;
 typedef unsigned long int uint32_t;
 #else
-typedef int int32_t;
 typedef unsigned int uint32_t;
 #endif
 
