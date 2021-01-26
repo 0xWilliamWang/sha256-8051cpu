b2ddad630ffd0c772b9908e1b09403a3983d7d0c
wangkaixuan <2654189525@qq.com>
2021-01-26 16:24:47 +0800

    add test_base_op

--- test.c
+++ test.c
@@ -4,12 +4,63 @@
 #include "sha256.h"
 #include "stdint.h"
 
-void test_rotate_left_right()
+void test_base_op()
 {
 	int i = 0;
 	uint32_t num[10] = {0};
 	uint32_t num1 = 0x12345678;
+	uint32_t num2 = 0x12328678;
+	uint32_t num3 = 0x28678678;
+	/*     CH       */
+	num[i++] = CH(num1, num2, num3);
+	assert(num[0] == 0x3A738678);
 
+	/*     MAJ       */
+	i=0;
+	num[i++] = MAJ(num1, num2, num3);
+	assert(num[0] == 0x12368678);
+
+	/*     DBL_INT_ADD       */
+	i=0;
+	DBL_INT_ADD(num[0], num2, num3);
+	assert(num[0] == 0x3A9E0CF0);
+
+	/*     EPx       */
+	i=0;
+	num[i++] = EP0(num1);
+	num[i++] = EP0(num2);
+	num[i++] = EP0(num3);
+	num[i++] = EP1(num1);
+	num[i++] = EP1(num2);
+	num[i++] = EP1(num3);
+
+	i=0;
+	assert(num[i++] == 0x66146474);
+	assert(num[i++] == 0xfd55d042);
+	assert(num[i++] == 0xa7c14203);
+	assert(num[i++] == 0x3561abda);
+	assert(num[i++] == 0x3609b040);
+	assert(num[i++] == 0x1c67aefd);
+
+	/*     SIGx       */
+	i=0;
+	num[i++] = SIG0(num1);
+	num[i++] = SIG0(num2);
+	num[i++] = SIG0(num3);
+	num[i++] = SIG1(num1);
+	num[i++] = SIG1(num2);
+	num[i++] = SIG1(num3);
+
+	i=0;
+	assert(num[i++] == 0xe7fce6ee);
+	assert(num[i++] == 0x53fc314f);
+	assert(num[i++] == 0x14c235da);
+	assert(num[i++] == 0xa1f78649);
+	assert(num[i++] == 0x13f787fe);
+	assert(num[i++] == 0x33f908de);
+	
+	/*     ROTATE       */
+	i=0;
 	num[i++] = ROTLEFT(num1, 4);
 	num[i++] = ROTLEFT(num1, 8);
 	num[i++] = ROTLEFT(num1, 12);
@@ -83,7 +134,6 @@ void test_data_type(void)
 	len[i++] = sizeof(uint8_t);
 	len[i++] = sizeof(uint32_t);
 
-	assert(len[0] == 4);
 	assert(len[1] == 1);
 	assert(len[2] == 4);
 }
@@ -128,7 +178,7 @@ int main()
 {
 	test_data_type();
 	test_uint32();
-	test_rotate_left_right();
+	test_base_op();
 	sha256_test1();
 	sha256_test();
 	printf("test passed!\n");
