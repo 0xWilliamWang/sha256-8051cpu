db0689b2fe708b4c1c6e01ecc4cf2c8feb33ff83
wangkaixuan <2654189525@qq.com>
2021-01-26 13:17:59 +0800

    add keil project file

--- sha256.c
+++ sha256.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdint.h>
+#include "stdint.h"
 #include "sha256.h"
 
 uint32_t k[64] = {
---
+++ stdint.h
@@ -0,0 +1,11 @@
+#ifndef STDINT__H
+#define STDINT__H
+
+typedef signed char int8_t;
+typedef int int16_t;
+typedef long int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned int uint16_t;
+typedef unsigned long int uint32_t;
+
+#endif
